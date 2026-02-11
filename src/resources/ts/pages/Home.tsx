c8c700238067fbd533d1ef0af3ea90b6b3d6e73c
hata-tomoyuki <h.tomo0602@gmail.com>
2026-02-11 11:05:56 +0900

    簡易実装: Home

--- src/resources/ts/pages/Home.tsx
+++ src/resources/ts/pages/Home.tsx
@@ -1,3 +1,46 @@
+import { useEffect, useState } from "react";
+
+interface Post {
+    id: string;
+    caption: string;
+    user_id: string;
+    created_at: string;
+    updated_at: string;
+}
+
 export default function Home() {
-    return <div>Home</div>;
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    useEffect(() => {
+        try {
+            const fetchPosts = async () => {
+                const response = await fetch("/api/posts");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data: Post[] = await response.json();
+                setPosts(data);
+            };
+            fetchPosts();
+        } catch (error) {
+            console.error("Error fetching posts:", error);
+        }
+    }, []);
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <p>{post.caption}</p>
+                        <small>
+                            Posted by User {post.user_id} on{" "}
+                            {new Date(post.created_at).toLocaleDateString()}
+                        </small>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
