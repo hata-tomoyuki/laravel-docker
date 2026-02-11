a0c060511f426d75d082b7e0df52de506f899454
hata-tomoyuki <h.tomo0602@gmail.com>
2026-02-11 10:41:56 +0900

    React TypeScriptセットアップ

---
+++ src/resources/ts/App.tsx
@@ -0,0 +1,12 @@
+import Test from "./components/Test";
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello React + Laravel!!!!!!</h1>
+            <Test />
+        </>
+    );
+};
+
+export default App;
---
+++ src/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel + React</title>
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/ts/main.tsx'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
